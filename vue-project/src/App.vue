47b8e793ade6ab23fd72104dc07124cd00de0644
Babatunde Ogunbiyi <greyw4300@gmail.com>
2024-08-06 18:55:33 +0100

    commit1

---
+++ vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<button @click='login'>click me</button>
+  <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+  
+</template>
+
+<script setup>
+import axios from 'axios' 
+
+const login = async () => {
+  
+//  await axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+//     // Login...
+//   });
+
+// await axios.post('http://localhost:8000/login',{email:'greyw4300@gmail.com', password:'password'})
+
+axios.get('http://localhost:8000/api/user').then((res => {console.log(res)}))
+
+}
+
+</script>
